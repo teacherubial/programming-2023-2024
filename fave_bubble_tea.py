b660e20a457a6eeec7385c3be6fb7718ed32a483
Tim Ubial <tubial@sd38.bc.ca>
2023-10-30 12:41:14 -0700

    changed bbt

--- fave_bubble_tea.py
+++ fave_bubble_tea.py
@@ -12,6 +12,7 @@ chatime_likes = 0
 suntea_likes = 0
 xingfutang_likes = 0
 bbqueen_likes = 0
+other_likes = 0
 
 for _ in range(NUM_RESPONDENTS):
     # Ask the user what their favourite bbt place is
@@ -27,7 +28,25 @@ for _ in range(NUM_RESPONDENTS):
         chatime_likes += 1
     elif fave_bbt == "suntea":
         suntea_likes += 1
+    elif fave_bbt == "xing fu tang":
+        xingfutang_likes += 1
+    elif fave_bbt == "bubble queen":
+        bbqueen_likes += 1
+    else:
+        other_likes += 1
 
+
+coco_percentage = coco_likes / NUM_RESPONDENTS * 100
+chatime_percentage = chatime_likes / NUM_RESPONDENTS * 100
+suntea_percentage = suntea_likes / NUM_RESPONDENTS * 100
+xingfutang_percentage = xingfutang_likes / NUM_RESPONDENTS * 100
+bbqueen_percentage = bbqueen_likes / NUM_RESPONDENTS * 100
+other_percentage = other_likes / NUM_RESPONDENTS * 100
 
 # Print a summary of the results
-print(f"CoCo Likes: {coco_likes}")
+print(f"CoCo Likes: {coco_likes} | {coco_percentage:.2f}%")
+print(f"Chatime Likes: {chatime_likes} | {chatime_percentage:.2f}%")
+print(f"Suntea Likes: {suntea_likes} | {suntea_percentage:.2f}%")
+print(f"Xing Fu Tang Likes: {xingfutang_likes} | {xingfutang_percentage:.2f}%")
+print(f"Bubble Queen Likes: {bbqueen_likes} | {bbqueen_percentage:.2f}%")
+print(f"Other Likes: {other_likes} | {other_percentage:.2f}%")
