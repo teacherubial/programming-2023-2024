5ae38d8f66d35fd305a7c67ef4da4035a3a23b1e
Tim Ubial <tubial@sd38.bc.ca>
2023-11-02 14:11:11 -0700

    chip rater update

--- chip_rater.py
+++ chip_rater.py
@@ -19,10 +19,21 @@ questions = [
     "From 1 to 5, how would you rate the packaging? 5 is I would buy this JUST for the packaging. 1 is Someone got paid to put this together?"
 ]
 
+total_rating = 0
+
 # For every question in that list
-    # Ask the question
+for question in questions:
+    print(question)
+
     # Get the user's rating
+    user_rating = int(input().strip(",.?! "))
+
     # Add the rating to some total-rating
+    total_rating += user_rating
 
 # Do some maths on the results
+# Use the average score to represent the chip's rating
+average_rating = total_rating / len(questions)
+
 # Present the results
+print(f"The average rating for this chip is: {average_rating:.2f} / 5")
