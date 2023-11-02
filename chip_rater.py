4508a44a47869ee04b3c34d0fe666f4e559c96e7
Tim Ubial <tubial@sd38.bc.ca>
2023-11-02 13:59:49 -0700

    added chip rater

---
+++ chip_rater.py
@@ -0,0 +1,28 @@
+# Chip Rater
+# Author: Ubial
+# 2 November 2023
+
+# Interview people about their
+# perception of the deliciousness
+# of potato chips.
+# We will ask them a set of
+# questions. In the end,
+# we'll give an aggregate score.
+
+# Greet the user
+print("Please answer the following questions on the chip you just ate.")
+
+# Create a list of questions to ask
+questions = [
+    "How crispy is the chip on a scale from 1 to 5? 5 is very crispy, 1 is mushy.",
+    "How would you rate the taste from 1 to 5? 5 is best chip ever. 1 is I'd rather eat dirt.",
+    "From 1 to 5, how would you rate the packaging? 5 is I would buy this JUST for the packaging. 1 is Someone got paid to put this together?"
+]
+
+# For every question in that list
+    # Ask the question
+    # Get the user's rating
+    # Add the rating to some total-rating
+
+# Do some maths on the results
+# Present the results
