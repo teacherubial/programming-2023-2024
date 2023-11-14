55292881020ed6d7c0d6523276a36b3583c3a748
Tim Ubial <tubial@sd38.bc.ca>
2023-11-14 11:36:02 -0800

    SFUs best sim score

---
+++ sim_score_sfu_best.py
@@ -0,0 +1,47 @@
+# SFUs Best - Similarity Score
+# Author: Ubial
+# 10 November 2023
+
+# Load data from a file
+# "Read" it in a meaningful
+# Link our Sim Score algo to the data
+
+# Open the file
+with open("./data.csv") as f:
+    f.readline()
+
+    # The second line of the csv file
+    f.readline()
+
+# Create a "profile" of likes (fave places
+# in SFU)
+profile = [
+    "Bubble World",
+    "Chef Hung",
+    "Uncle Fatih's",
+    "Guadalupe (MBC)",
+    "Steve's Poke Bar"
+]
+
+with open("./data.csv") as f:
+    # Throw away the header
+    header = f.readline()
+
+    for line in f:
+        # Convert the string to a list
+        current_likes = line.split(",")
+
+        # Store the person's name
+        current_name = current_likes[1]
+        
+        # Initialize the current sim score
+        current_sim_score = 0
+
+        # Sim score algorithm
+        for item in profile:
+            if item in current_likes:
+                current_sim_score += 1
+        
+        # Print the results from this line of data
+        print(f"{current_name} - Score: {current_sim_score}")
+
