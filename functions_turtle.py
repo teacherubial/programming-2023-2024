f12271ae7c27643ba0960fd6556fac0d5e30498e
Tim Ubial <tubial@sd38.bc.ca>
2023-12-04 10:37:59 -0800

    recursion with alfredriverwilson

--- functions_turtle.py
+++ functions_turtle.py
@@ -20,10 +20,41 @@ def draw_square(side_length: float) -> None:
         alfred_river_wilson.left(90)
 
 
-alfred_river_wilson.speed(0)
+def draw_tree(level: int, height: int) -> None:
+    """A recursive function that draws a tree
+    with initial given height
 
-# Create a squares that get bigger and bigger
-for i in range(50):
-    draw_square(squared(i))
+    Params:
+
+    level - number representing levels of branches
+    height - height of the main trunk in pixels
+    """
+
+    if level > 0:
+        # 1. Draw the branch
+        alfred_river_wilson.forward(height)
+
+        # 2. Turn to the left
+        alfred_river_wilson.left(39)
+
+        # 3. Draw a smaller tree (current level - 1)
+        draw_tree(level - 1, height / 1.5)
+
+        # 4. Turn to the right
+        alfred_river_wilson.right(39 * 2)
+
+        # 5. Draw a smaller tree (current level - 1)
+        draw_tree(level - 1, height / 1.5)
+
+        # 6. Move back to beginning
+        alfred_river_wilson.left(39)
+        alfred_river_wilson.back(height)
+    else:
+        # create a level 0 tree, which is a leaf
+        original_colour = alfred_river_wilson.color()
+        alfred_river_wilson.color("green")
+        alfred_river_wilson.stamp()
+        alfred_river_wilson.color(original_colour)
+
 
 turtle.done()
