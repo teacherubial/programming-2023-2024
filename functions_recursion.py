e3e5ab995c2dc32e33f946b511fa67a9a73f3ae5
Tim Ubial <tubial@sd38.bc.ca>
2023-12-08 10:35:00 -0800

    recursion vs iteration

--- functions_recursion.py
+++ functions_recursion.py
@@ -2,6 +2,8 @@
 # Author: Ubial
 # 7 December 2023
 
+import time
+
 
 def factorial(n: int) -> int:
     """Return the nth factorial.
@@ -25,5 +27,32 @@ def fib(n: int) -> int:
         return fib(n - 1) + fib(n - 2)
 
 
+def fib_itr(n: int) -> int:
+    """Returns the nth Fibonacci number.
+    Calculated iteratively."""
+    last_num = 0
+    num = 1
+    result = 1
+
+    for i in range(n - 1):
+        result = num + last_num
+
+        num, last_num = result, num
+
+    return result
+
+
 # print(factorial(100))
-print(fib(20))
+print(fib(5), fib_itr(5))
+
+time_initial = time.perf_counter()
+fib(40)
+time_final = time.perf_counter()
+
+print(f"Recursive: {time_final-time_initial}")
+
+time_initial = time.perf_counter()
+fib_itr(40)
+time_final = time.perf_counter()
+
+print(f"Iterative: {time_final-time_initial}")
