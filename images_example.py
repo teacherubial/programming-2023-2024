663d6eedec3c5667316dd43bfd2246bd7dbd1903
Tim Ubial <tubial@sd38.bc.ca>
2023-12-14 12:00:59 -0800

    pixellllls

--- images_example.py
+++ images_example.py
@@ -4,9 +4,37 @@
 
 from PIL import Image
 
+
+def pixel_to_name(pixel: tuple) -> str:
+    """Given a 3-tuple, return a string representing
+    its colour
+
+    Params:
+        pixel = 3-tuple of values (red, green, blue)
+
+    Returns:
+        name of the colour
+    """
+    red, green, blue = pixel
+
+    if red < 25 and blue < 25 and green > 249:
+        return "green"
+    else:
+        return "colour unknown"
+
+
 # open up kid green
 with Image.open("./Images/kid-green.jpg") as im:
-    # grab the pixel in the top left corner
-    pixel = im.getpixel((0, 0))
-    # print the pixel information
-    print(pixel)
+    # create variables that store the width and height
+    image_height = im.height
+    image_width = im.width
+
+    # open background image
+
+    # starting at the top and working our way down
+    # visit the pixels from left to right
+    for y in range(image_height):
+        for x in range(image_width):
+            pixel = im.getpixel((x, y))
+
+            print(pixel_to_name(pixel))
