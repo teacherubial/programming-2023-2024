a28fd47dd67f52fe7a71eeaa46df7778b43a0894
Tim Ubial <tubial@sd38.bc.ca>
2024-01-09 12:25:04 -0800

    colour_helper update

--- colour_helper.py
+++ colour_helper.py
@@ -2,6 +2,31 @@
 # Functions that help wither colours
 
 
+def pixel_to_name(pixel: tuple) -> str:
+    """Given a 3-tuple, return a string representing
+    its colour
+
+    Params:
+        pixel = 3-tuple of values (red, green, blue)
+
+    Returns:
+        name of the colour
+    """
+    red, green, blue = pixel
+
+    # TODO: detect red pixels
+    if red < 200 and blue < 200 and green > 220:
+        return "green"
+    elif red > 170 and green < 60 and blue < 60:
+        return "red"
+    else:
+        return "colour unknown"
+
+
+print(pixel_to_name((180, 3, 2)))
+print(pixel_to_name((255, 255, 255)))
+
+
 def is_light(pixel: tuple) -> bool:
     """Returns True if the pixel is a "light" pixel
 
