853bcbca381b4a5543ec49e2b70db60b1ae5de69
Tim Ubial <tubial@sd38.bc.ca>
2024-01-09 12:44:50 -0800

    bean counter

---
+++ bean_counter.py
@@ -0,0 +1,38 @@
+# Jelly Bean Colour Counter
+# Author: Ubial
+# 9 January 2024
+
+# Version 0.1
+#  - Count red pixels/beans
+#  - Report on the percentage of all red beans
+
+from PIL import Image
+
+import colour_helper
+
+jelly_bean_img = Image.open("./Images/Jelly Beans.jpg")
+
+red_pixels = []
+
+# Visit every pixel in the image
+for y in range(jelly_bean_img.height):
+    for x in range(jelly_bean_img.width):
+        # Get the pixel information
+        current_pixel = jelly_bean_img.getpixel((x, y))
+
+        # If this pixel is red
+        if colour_helper.pixel_to_name(current_pixel) == "red":
+            # Store its location somewhere
+            red_pixels.append((x, y))
+
+# Count all the locations of red pixels
+red_pixel_count = len(red_pixels)
+total_pixels = jelly_bean_img.width * jelly_bean_img.height
+
+# Divide by the total pixels in the image
+red_pixel_percentage = red_pixel_count / total_pixels * 100
+
+# Generate the report
+print(f"Red Jelly Beans: {round(red_pixel_percentage, 2)}%")
+
+jelly_bean_img.close()
