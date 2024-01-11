bd1543ef0075d2f60d00133260208767705be7a8
Tim Ubial <tubial@sd38.bc.ca>
2024-01-11 10:41:24 -0800

    pixel map added

--- bean_counter.py
+++ bean_counter.py
@@ -10,6 +10,8 @@ from PIL import Image
 
 import colour_helper
 
+RED_PIXEL = (160, 0, 0)
+
 jelly_bean_img = Image.open("./Images/Jelly Beans.jpg")
 
 red_pixels = []
@@ -24,6 +26,23 @@ for y in range(jelly_bean_img.height):
         if colour_helper.pixel_to_name(current_pixel) == "red":
             # Store its location somewhere
             red_pixels.append((x, y))
+
+# Create a map of all red pixels "found"
+# Create a new image that stores the map
+#   - the new image should be the same size as original
+orig_image_width = jelly_bean_img.width
+orig_image_height = jelly_bean_img.height
+
+red_pixel_map = Image.new("RGB", (orig_image_width, orig_image_height))
+
+# For every pixel location in the red_pixels list
+# Place a red pixel at that location
+for pixel_loc in red_pixels:
+    red_pixel_map.putpixel(pixel_loc, RED_PIXEL)
+
+# Save the image
+red_pixel_map.save("./Images/red_pixel_map.jpg")
+red_pixel_map.close()
 
 # Count all the locations of red pixels
 red_pixel_count = len(red_pixels)
